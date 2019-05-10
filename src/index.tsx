623631bccdf3ea98579e64515f1aaac7c770a3bf
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2019-05-10 10:14:45 +0200

    passing data throuth props

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+class Square extends React.Component<{ value: number }> {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -14,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i: number) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
