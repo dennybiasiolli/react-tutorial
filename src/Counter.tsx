3631ed50e2b29d9c7c5a91f9df544c9000758242
Denny Biasiolli <denny.biasiolli@gmail.com>
2019-05-10 14:32:51 +0200

    new example using effetct hook

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 export function Counter() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+
+    return () => {
+      document.title = 'Counter unmounted';
+    }
+  });
 
   return (
     <div>
