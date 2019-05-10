16ccba044021e8193cd1ad0e3d8d8ccaab08c44c
Denny Biasiolli <denny.biasiolli@gmail.com>
2019-05-10 12:43:52 +0200

    changing Board to function component

--- src/index.tsx
+++ src/index.tsx
@@ -13,34 +13,32 @@ function Square(props: { value?: string, onClick: Function }) {
   );
 }
 
-class Board extends React.Component<{ squares: string[], onClick: Function }> {
-  renderSquare(i: number) {
-    return <Square
-      value={this.props.squares[i]}
-      onClick={() => this.props.onClick(i)}
-    />;
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+function Board(props: { squares: string[], onClick: Function }) {
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+
+  function renderSquare(i: number) {
+    return <Square
+      value={props.squares[i]}
+      onClick={() => props.onClick(i)}
+    />;
   }
 }
 
