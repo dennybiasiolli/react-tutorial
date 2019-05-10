f0de0706c2209c52e08630791caf8d18c4498617
Denny Biasiolli <denny.biasiolli@gmail.com>
2019-05-10 12:15:13 +0200

    declaring a winner

--- src/index.tsx
+++ src/index.tsx
@@ -23,14 +23,15 @@ class Board extends React.Component<{}, { squares: string[], xIsNext: boolean }>
   }
 
   handleClick(i: number) {
-    if (this.state.squares[i] === null) {
-      const squares = this.state.squares.slice();
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-      this.setState({
-        squares,
-        xIsNext: !this.state.xIsNext,
-      });
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
     }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i: number) {
@@ -41,7 +42,13 @@ class Board extends React.Component<{}, { squares: string[], xIsNext: boolean }>
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -88,3 +95,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+
+function calculateWinner(squares: string[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (const [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
