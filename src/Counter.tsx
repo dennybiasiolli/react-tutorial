080aa0d7f69b49f9f0ef200b4b7f20e0fa9f00a9
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2019-05-11 10:23:21 +0200

    Counter: using shared store instead of local state

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,10 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Dispatch, bindActionCreators } from 'redux';
+
+import { RootState } from './store';
+import { increment, decrement } from './store/reducers/counter-actions';
 
 
-export function Counter() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
+const mapStateToProps = (state: RootState) => ({
+  count: state.counter.count,
+});
 
+const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators({
+  setIncrement: increment,
+  setDecrement: decrement,
+}, dispatch);
+
+export interface CounterProps extends ReturnType<typeof mapStateToProps>, ReturnType<typeof mapDispatchToProps> { };
+
+function CounterBase({ count, setIncrement, setDecrement }: CounterProps) {
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     // Update the document title using the browser API
@@ -18,9 +31,13 @@ export function Counter() {
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+      <button onClick={() => setIncrement(1)}>+</button>
+      <button onClick={() => setDecrement(1)}>-</button>
     </div>
   )
 }
+
+export const Counter = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(CounterBase)
